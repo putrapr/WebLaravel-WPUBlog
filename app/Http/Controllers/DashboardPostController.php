533894047333ccbec5bf15649933ee1d2ce42d96
Putra Prasetya <putraprasetya333@gmail.com>
2023-06-13 20:44:40 +0700

    Feature: Preview, Update, & Delete Image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardPostController extends Controller {
@@ -53,6 +54,8 @@ class DashboardPostController extends Controller {
     $validatedData['user_id'] = auth()->user()->id;
     $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
+
+
     Post::create($validatedData);
     return redirect('/dashboard/posts')->with('success', 'Now post has been added!');
   }
@@ -93,6 +96,7 @@ class DashboardPostController extends Controller {
     $rules = [
       'title' => 'required|max:255',
       'category_id' => 'required',
+      'image' => 'image|file|max:1024',
       'body' => 'required'
     ];
 
@@ -101,6 +105,13 @@ class DashboardPostController extends Controller {
     }
 
     $validatedData = $request->validate($rules);
+
+    if ($request->file('image')) {
+      if ($request->oldImage) {
+        Storage::delete($request->oldImage);
+      }
+      $validatedData['image'] = $request->file('image')->store('post-images');
+    }
 
     $validatedData['user_id'] = auth()->user()->id;
     $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -118,9 +129,11 @@ class DashboardPostController extends Controller {
    * @return \Illuminate\Http\Response
    */
   public function destroy(Post $post) {
+    if ($post->image) {
+      Storage::delete($post->image);
+    }
     Post::destroy($post->id);
     return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
-
   }
 
   public function checkSlug(Request $request){
