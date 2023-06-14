78a8c0413352e3a0eb6848877b3f6bb893d09fe6
Putra Prasetya <putraprasetya333@gmail.com>
2023-06-14 22:46:15 +0700

    Feature: Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,28 +2,31 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register() {
+      //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-      Paginator::useBootstrap();
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot() {
+    Paginator::useBootstrap();
+
+    Gate::define('admin', function(User $user){
+      return $user->is_admin;
+    });
+  }
 }
